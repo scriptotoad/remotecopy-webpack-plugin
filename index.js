44b7d167651ff6f618c5d822e1198bd071c79c6a
scriptotoad <galexey@prototypes.ru>
2016-09-15 16:10:32 +0300

    fix moment when make mkdir

--- index.js
+++ index.js
@@ -19,6 +19,70 @@ function execp(s){
     });
 }
 
+const workingDirectory = process.cwd();
+//массив путей, начиная с наиболее коротких, затем те которые их продолжают (содержат их в начале)
+//затем новый кратчайший путь, не начинающийся с выше записанными
+//в записи "путь" лежат имена файлов этого пути
+/**
+ * @typedef {Array.<Array.<string>>} NestingOrderRec
+ * @param {Array.<string>} NestingOrderRec[0] весь массив это путь до файлов, где каждый елемент название папки
+ * @param {Array.<string>} NestingOrderRec[1] список имен файлов, лежащих в этой папке
+ */
+var NestingOrderRec;
+/**
+ * @param {Array.<string>} data unsorted list of pathname
+ * @param {string} outputPath absolute path to root of output
+ * @return {Array.<NestingOrderRec>}
+ */
+function sortingByNestingOrder(data, outputPath){
+    return data.reduce( (result, file, i, a) => {
+        var relativeLocalPathName = path.relative(
+            workingDirectory,
+            path.join(
+                outputPath,
+                file.replace(/\?.*$/,'')
+            )
+        );
+        var pathNameChunks = relativeLocalPathName.split(path.sep);
+        var pathChunks = pathNameChunks.slice(0, -1);
+        var nearest = [0];
+        var record = result.find(
+            (rec, idx) => {
+                var comparedCount = rec[0].slice(0, pathChunks.length)
+                .concat(null) //грязный хук чоб проще вырезать нужную часть
+                .findIndex(
+                    (savedPathChunk, i) => savedPathChunk !== pathNameChunks[i]
+                );
+                if( nearest[0] < comparedCount ){
+                    nearest = [comparedCount, idx, idx];
+                } else if ( nearest[0] == comparedCount ){
+                    nearest[2] = idx;
+                }
+                return comparedCount === pathChunks.length
+            }
+        );
+        
+        if( !record || result[nearest[1]][0].length > pathChunks.length ){
+            if( nearest[0] > 0 ){
+                record = [pathChunks, []];
+                if( result[nearest[1]][0].length > pathChunks.length ){
+                    result.splice( nearest[1], 0, record );
+                }
+                if( result[nearest[2]][0].length < pathChunks.length ){
+                    result.splice( nearest[2] + 1, 0, record );
+                }
+            } else {
+                record = [pathChunks, []];
+                result.push(record);
+            }
+        }
+        record[1].push( pathNameChunks.slice(-1)[0] );
+        return result
+    }, []);
+}
+/**
+ * @param {{remoteOutputAddress: string}} options
+ */
 function RemoteCopyPlugin(options) {
     if(!options.remoteOutputAddress){
         throw new Error('RemoteCopyPlugin: remoteOutputAddress must be present');
@@ -31,7 +95,6 @@ function RemoteCopyPlugin(options) {
         // TODO https://github.com/ironSource/node-regedit 
         // cmd REG QUERY HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\PuTTY_is1 /v InstallLocation
         // http://forum.sysinternals.com/finding-all-installed-programs-from-the-registry_topic21312.html
-        const workingDirectory = process.cwd();
         compiler.plugin('after-emit', (compilation, callback) => {
             const remoteOutputAddress = options.remoteOutputAddress;
             var hostName = remoteOutputAddress.split(':');
@@ -40,53 +103,7 @@ function RemoteCopyPlugin(options) {
             const port = options.port ? '-P ' + options.port + ' ': '';
             const outputPath = compiler.outputPath;
             
-            //массив путей, начиная с наиболее коротких, затем те которые из продолжают (содержат их в начале)
-            //затем новый кратчайший путь, не начинающийся с выше записанными
-            //в записи "путь" лежат имена файлов этого пути
-            var records = Object.keys(compilation.assets).reduce( (result, file, i, a) => {
-                var relativeLocalPathName = path.relative(
-                    workingDirectory,
-                    path.join(
-                        outputPath,
-                        file.replace(/\?.*$/,'')
-                    )
-                );
-                var pathNameChunks = relativeLocalPathName.split(path.sep);
-                var pathChunks = pathNameChunks.slice(0, -1);
-                var nearest = [0];
-                var record = result.find(
-                    (rec, idx) => {
-                        var comparedCount = rec[0].slice(0, pathChunks.length)
-                        .concat(null) //грязный хук чоб проще вырезать нужную часть
-                        .findIndex(
-                            (savedPathChunk, i) => savedPathChunk !== pathNameChunks[i]
-                        );
-                        if( nearest[0] < comparedCount ){
-                            nearest = [comparedCount, idx, idx];
-                        } else if ( nearest[0] == comparedCount ){
-                            nearest[2] = idx;
-                        }
-                        return comparedCount === pathChunks.length
-                    }
-                );
-                
-                if( !record || result[nearest[1]][0].length > pathChunks.length ){
-                    if( nearest[0] > 0 ){
-                        record = [pathChunks, []];
-                        if( result[nearest[1]][0].length > pathChunks.length ){
-                            result.splice( nearest[1], 0, record );
-                        }
-                        if( result[nearest[2]][0].length < pathChunks.length ){
-                            result.splice( nearest[2] + 1, 0, record );
-                        }
-                    } else {
-                        record = [pathChunks, []];
-                        result.push(record);
-                    }
-                }
-                record[1].push( pathNameChunks.slice(-1)[0] );
-                return result
-            }, []);
+            var records = sortingByNestingOrder( Object.keys(compilation.assets), outputPath );
             records.reverse();
             
             // длина командной строки не более 2047 символов https://support.microsoft.com/ru-ru/kb/830473
@@ -94,19 +111,27 @@ function RemoteCopyPlugin(options) {
             // TcpNumConnections ? https://support.microsoft.com/ru-ru/kb/314053#mt2
             
             const MAX_COMMAND_LENGTH = 2047;
-            var lastFirstPathChunk;
             const MAX_CONNECT_COUNT = 5;
-            var pDirs = new Map();
+            var longestPathsMap = new Map();
+            //http://stackoverflow.com/questions/19393073/map-collection-in-nodejs-harmony-es6-broken#answer-33726872
+            var longestPathsMapKeys = [];
             var currentConnections = 0;
             var recordIndex = 0;
             function sendMaxParallelCommand(){
-                var record;
+                var record, longestPathKey;
                 for (; MAX_CONNECT_COUNT > currentConnections && recordIndex < records.length;){
                     record = records[recordIndex++];
-                    if( !pDirs.has(record[0][0]) ){
+                    longestPathKey = longestPathsMapKeys.find(
+                        pathChunks => !record[0].find(
+                            (chunk, i) => chunk !== pathChunks[i]
+                        )
+                    );
+                    if( !longestPathKey ){
                         currentConnections++;
-                        pDirs.set(
-                            record[0][0],
+                        longestPathKey = record[0];
+                        longestPathsMapKeys.push(longestPathKey);
+                        longestPathsMap.set(
+                            longestPathKey,
                             execp(
                                 puttyInstallLocation + 'plink '
                                 + port
@@ -118,7 +143,7 @@ function RemoteCopyPlugin(options) {
                             )
                         );
                     }
-                    pDirs.get(record[0][0]).then(
+                    longestPathsMap.get(longestPathKey).then(
                         (record => () => {
                             var res = [
                                puttyInstallLocation, 'pscp ', port,
@@ -167,7 +192,7 @@ function RemoteCopyPlugin(options) {
                     );
                 }
             }
-            sendMaxParallelCommand();            
+            sendMaxParallelCommand();
         });
     }
     return {
